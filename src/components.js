d2cd6ecc81afeb6441e1826a0a3b0c557d0d2e38
AndSelm <and.selm+git@gmail.com>
2017-12-01 18:24:19 +0100

    FIX: adding `opensearch-launcher` command

--- src/components.js
+++ src/components.js
@@ -1,16 +1,44 @@
-export default [ {
-  name: 'opensearch',
-  description: 'opensearch main component',
-  hidden: true,
-  graph: {
-    suggestions$: [
-      { 'from-input': 'query$' },
-      { operator: 'opensearch.getSuggestions' },
-      {
-        operator: 'startWith',
-        args: [ [] ]
-      },
-      { 'to-output': 'suggestions$' }
-    ]
+export default [
+  {
+    name: 'opensearch',
+    description: 'opensearch main component',
+    hidden: true,
+    graph: {
+      suggestions$: [
+        { 'from-input': 'query$' },
+        { operator: 'opensearch.getSuggestions' },
+        {
+          operator: 'startWith',
+          args: [ [] ]
+        },
+        { 'to-output': 'suggestions$' }
+      ]
+    }
+  },
+
+  {
+    name: 'opensearch-launcher',
+    description: 'opensearch main command',
+    hidden: true,
+    subgraphs: [
+      'opensearch',
+      'launcher'
+    ],
+    graph: {
+      'connect-query': [
+        { 'from-subgraph-output': 'launcher::query$' },
+        { 'to-subgraph-input': 'opensearch::query$' }
+      ],
+      'connect-data': [
+        { 'from-subgraph-output': 'opensearch::suggestions$' },
+        { 'to-subgraph-input': 'launcher::data$' }
+      ],
+      'connect-execution': [
+        { 'from-subgraph-output': 'launcher::chosenListenItem$' },
+        { operator: 'launcher.listview.item.getURL' },
+        { operator: 'launcher.openExternal' },
+        { operator: 'subscribe' }
+      ]
+    }
   }
-} ];
+];
