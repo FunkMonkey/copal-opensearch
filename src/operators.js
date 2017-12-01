31d0fa1911b21f7c1bf7222aa72fc435cc07416d
AndSelm <and.selm+git@gmail.com>
2017-12-01 15:15:45 +0100

    FIX: determine service from command data

--- src/operators.js
+++ src/operators.js
@@ -5,11 +5,11 @@ const SUGGESTION_TYPE = 'application/x-suggestions+json';
 
 export default function ( copalOpensearch ) {
   return {
-    'opensearch.getSuggestions': ( [ query$ ], config, { data } ) =>
+    'opensearch.getSuggestions': ( [ query$ ], config, command ) =>
       query$
         .filter( searchTerms => searchTerms !== '' )
         .flatMap( searchTerms => {
-          const service = copalOpensearch.services.google;
+          const service = copalOpensearch.services[ command.data.opensearch.service ];
           return Rx.Observable.fromPromise( service.search( { searchTerms }, SUGGESTION_TYPE ) )
                               .map( response => response[1] );
         } )
